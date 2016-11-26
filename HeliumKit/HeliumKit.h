09e46cc9b0556dbd9f7c3834496966051f67056b
elkraneo <elkraneo@icloud.com>
2016-11-26 13:34:08 +0100

    Sets the initial setup

---
+++ HeliumKit/HeliumKit.h
@@ -0,0 +1,19 @@
+//
+//  HeliumKit.h
+//  HeliumKit
+//
+//  Created by elkraneo on 26/11/2016.
+//  Copyright © 2016 metodowhite. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HeliumKit.
+FOUNDATION_EXPORT double HeliumKitVersionNumber;
+
+//! Project version string for HeliumKit.
+FOUNDATION_EXPORT const unsigned char HeliumKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HeliumKit/PublicHeader.h>
+
+
